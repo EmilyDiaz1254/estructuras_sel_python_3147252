a3bb49b2299b1d478ca5c52c34a857aa55bdaaac
Aprendiz <felipesanchezcon2@gmail.com>
2025-03-10 11:24:25 -0500

    Ejercicios resueltos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -18,3 +18,28 @@ print("El resultado de operar con and es" ,y)
 #Ejemplo 3: Operador or
 y=False or False
 print("el resultado de operar con or es" ,y)
+
+
+'''
+Jerarquia de predencia de operadores
+(logicoa inclusive)
+1.  ()
+2.  **
+3.  *,/,%
+4.  +,-
+5.  >,<,>=,<=,!=,==
+6.  not
+7.  and
+8.  or
+'''
+
+#Ejemplo 4:Jerarquia de operadores 
+y = False and not True or False
+print("el resultado de operar con jerarquia de operadores es", y)
+
+
+
+#Ejemplo 5: operadores relacionales y logicos
+y = not 3 > 4 and 4 -- 4 or 3<2
+print("el resultado de operar con jerarquia de o", y)
+
